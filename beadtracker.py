6527bb4570ffb5b1d5b1f9bfb4caa25e59e4272d
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-07-18 12:19:17 +0430

    Add bulk_bead_finder function
    It find beads for bulk of image instead of one

--- beadtracker.py
+++ beadtracker.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
 
-from beadfinder import read_image
-from _type_hints import path_list, image_list
+import numpy as np
+
+from beadfinder import bead_finder, get_beads, read_image
+from _type_hints import path_list, image_list, beads_pack
 
 
 def read_images(paths: path_list) -> image_list:
     return [read_image(path) for path in paths]
+
+
+def bulk_bead_finder(images: image_list, tau: int, min_pixels: int) -> beads_pack:
+    beads_pack = []
+    for img in images:
+        blobs = bead_finder(img, tau)
+        beads_pack.append(get_beads(min_pixels, blobs, as_numpy=True))
+
+    return np.array(beads_pack, dtype=np.object_)
